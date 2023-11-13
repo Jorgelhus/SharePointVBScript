f22cb473dd81a7581a17c76dec8f8658710abd47
Jorge Rocha <jorgemfrocha20@gmail.com>
2023-11-13 15:35:33 -0500

    Update CaptureDriveID.py
    Changed the output to show all drive IDs from the site.

--- CaptureDriveID.py
+++ CaptureDriveID.py
@@ -19,48 +19,45 @@ Script Variables:
 import requests
 import json
 
-# Define your Azure AD app registration details
-client_id = "YOUR_CLIENT_ID"  # Replace with your Azure AD app client ID
-client_secret = "YOUR_CLIENT_SECRET"  # Replace with your Azure AD app client secret
-tenant_id = "YOUR_TENANT_ID"  # Replace with your Azure AD tenant ID
+# Your Azure AD app registration details
+client_id = "YOUR_CLIENT_ID"
+client_secret = "YOUR_CLIENT_SECRET"
+tenant_id = "YOUR_TENANT_ID"
 
-# Define your SharePoint site ID
-site_id = "YOUR_SITE_ID"  # Replace with your SharePoint site ID
+# Your SharePoint site ID
+site_id = "YOUR_SITE_ID"
 
-try:
-    # Get an access token using client credentials flow
-    token_url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
-    token_data = {
-        "grant_type": "client_credentials",
-        "scope": "https://graph.microsoft.com/.default",
-        "client_id": client_id,
-        "client_secret": client_secret,
-    }
-    token_response = requests.post(token_url, data=token_data)
-    access_token = token_response.json().get("access_token")
+# Get an access token using client credentials flow
+token_url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
+token_data = {
+    "grant_type": "client_credentials",
+    "scope": "https://graph.microsoft.com/.default",
+    "client_id": client_id,
+    "client_secret": client_secret,
+}
+token_response = requests.post(token_url, data=token_data)
+access_token = token_response.json().get("access_token")
 
-    # Define the URL for listing drives in the site
-    list_drives_url = f"https://graph.microsoft.com/v1.0/sites/{site_id}/drives"
+# Microsoft Graph API endpoint to get drives (document libraries) in the site
+list_drives_url = f"https://graph.microsoft.com/v1.0/sites/{site_id}/drives"
 
-    # Set up the request with the access token
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
+# Headers for the request
+headers = {
+    "Authorization": f"Bearer {access_token}",
+    "Content-Type": "application/json",
+}
 
-    # Send the request
-    response = requests.get(list_drives_url, headers=headers)
+# Make the request to get the list of drives
+drives_response = requests.get(list_drives_url, headers=headers)
 
-    if response.status_code == 200:
-        # Parse the JSON response
-        data = response.json()
-        
-        # Extract information
-        drive_id = data['value'][0]['id']
+# Check if the request was successful (status code 200)
+if drives_response.status_code == 200:
+    drives_data = drives_response.json()
 
-        # Format and print the result
-        formatted_output = f'drive-id: {drive_id}'
-        print(formatted_output)
-    else:
-        print(f"Request failed with status code: {response.status_code}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+    # Extract and print the IDs of the drives (document libraries)
+    for drive in drives_data['value']:
+        print(f"Drive Name: {drive['name']}, Drive ID: {drive['id']}")
+else:
+    print(f"Failed to retrieve drives. Status code: {drives_response.status_code}")
+    print("Response content:")
+    print(drives_response.text)
